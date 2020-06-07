db0655ed75501e3eb994bf6747c683eaa9d560c1
roukell <62136980+roukell@users.noreply.github.com>
2020-06-08 02:37:48 +1000

    upload

--- script.js
+++ script.js
@@ -1,53 +1,157 @@
 const questionText = document.getElementById("question-text");
-const questionIndex = Math.round(Math.random() * 10/4);
 const optionBox = document.querySelector(".option-box");
+const currentQuestionNum = document.querySelector(".current-question-num");
+const answerDescription = document.querySelector(".answer-description");
+const nextQuestionBtn = document.querySelector(".next-question-btn");
+const correctAnswers = document.querySelector(".correct-answers");
+const seeResultBtn = document.querySelector(".see-result-btn");
+const remainingTime = document.querySelector(".remaining-time");
+const timeUpText = document.querySelector(".time-up-text");
+const quizHomeBox = document.querySelector(".quiz-home-box");
+const quizBox = document.querySelector(".quiz-box");
+const quizOverBox = document.querySelector(".quiz-over-box");
+const startAgainQuizBtn = document.querySelector(".start-again-quiz-btn");
+const goHomeBtn = document.querySelector(".go-home-btn");
+const startQuizBtn = document.querySelector(".start-quiz-btn");
 
-const myApp = [
-    {
-        question: "Name the smallest type of tree in the world.",
-        options: ["Cork tree","Bonsai tree","Hazel","Black walnut"],
-        answer: 1,
-    }, {
-        question: "Webpages are written in?",
-        options: ["FTP","HTML","HTTP","URL"],
-        answer: 1,
-    }, {
-        question: "C is?",
-        options: ["A third generation of high level langauge","A machine langauge","An assembly langauge","All of above"],
-        answer: 0,
-    }
-]
+let attempt = 0;
+let questionIndex = 0;
+let score = 0;
+let number = 0;
+let myArray = [];
+let interval;
+
+const myApp = [{
+    question: "Name the smallest type of tree in the world.",
+    options: ["Cork tree", "Bonsai tree", "Hazel", "Black walnut"],
+    answer: 1,
+    description: "Ha",
+}, {
+    question: "Webpages are written in?",
+    options: ["FTP", "HTML", "HTTP", "URL"],
+    answer: 1,
+    description: "Ho",
+}, {
+    question: "C is?",
+    options: ["A third generation of high level langauge", "A machine langauge", "An assembly langauge", "All of above"],
+    answer: 0,
+    description: "He",
+}]
+
 
 function load() {
-//console.log("test");
- questionText.innerHTML = myApp[questionIndex].question;
- createOptions();
+    //console.log("test");
+    number++;
+    questionText.innerHTML = myApp[questionIndex].question;
+    createOptions();
+    scoreBoard();
+    currentQuestionNum.innerHTML = number + " / " + myApp.length;
 }
 
 function createOptions() {
+    optionBox.innerHTML = "";
     for (let i = 0; i < myApp[questionIndex].options.length; i++) {
         // console.log(myApp[questionIndex].options[i]);
         const option = document.createElement("div");
         option.innerHTML = myApp[questionIndex].options[i];
         option.classList.add("option");
         option.id = i;
-        option.setAttribute("onclick","check(this)");
+        option.setAttribute("onclick", "check(this)");
         optionBox.appendChild(option);
     }
 }
 
+function generateRandomQuestion() {
+    let randomNumber = Math.floor(Math.random() * (myApp.length));
+    let hitDuplicate = 0;
+    if (myArray.length == 0) {
+        questionIndex = randomNumber;
+    } else {
+        for (let i = 0; i < myArray.length; i++) {
+            if (randomNumber == myArray[i]) {
+                hitDuplicate = 1;
+                // console.log("found duplicate")
+            }
+        }
+        if (hitDuplicate == 1) {
+            generateRandomQuestion();
+            return;
+        } else {
+            questionIndex = randomNumber;
+        }
+    }
+    
+    myArray.push(randomNumber);
+    // console.log(myArray);
+    load();
+
+}
+
 function check(ele) {
     // console.log(ele.id);
-    const id=ele.id;
+    const id = ele.id;
     if (id == myApp[questionIndex].answer) {
         // console.log("correct");
         ele.classList.add("correct");
+        score++;
+        scoreBoard();
     } else {
         // console.log("wrong");
         ele.classList.add("wrong");
+        // show correct answer when clicked answer is wrong;
+        for (let i = 0; i < optionBox.children.length; i++){
+            if(optionBox.children[i].id == myApp[questionIndex].answer) {
+                optionBox.children[i].classList.add("show-correct");
+            }
+        }
     }
-
+    attempt++;
     disableOptions();
+    showAnswerDescription();
+    showNextQuestionBtn();
+    stopTimer();
+
+    if (number / (myApp.length) == 1) {
+        // console.log("over");
+        quizOver();
+    }
+}
+
+function timeIsUp() {
+   showTimeUpText();
+    for (let i = 0; i < optionBox.children.length; i++){
+        if(optionBox.children[i].id == myApp[questionIndex].answer) {
+            optionBox.children[i].classList.add("show-correct");
+        }
+    }
+    disableOptions();
+    showAnswerDescription();
+    showNextQuestionBtn();
+    
+}
+
+function startTimer() {
+    let timeLimit = 16;
+    remainingTime.classList.remove("less-time");
+    interval = setInterval(() => {
+        timeLimit--;
+        if (timeLimit <10) {
+            timeLimit = "0" + timeLimit;
+        }
+        if (timeLimit < 6) {
+            remainingTime.classList.add("less-time");
+        }
+
+        remainingTime.innerHTML = timeLimit;
+        if (timeLimit == 0) {
+            clearInterval(interval);
+            timeIsUp();
+        }
+    },1000)
+}
+
+function stopTimer() {
+    clearInterval(interval);
 }
 
 function disableOptions() {
@@ -56,6 +160,103 @@ function disableOptions() {
     }
 }
 
-window.onload=()=>{
-    load();
+function showAnswerDescription() {
+    // answer description will only show if it is definied
+    if (typeof myApp[questionIndex].description !== "undefined") {
+        answerDescription.classList.add("show");
+        answerDescription.innerHTML = myApp[questionIndex].description;
+    }
 }
+
+function hideAnswerDescription() {
+    answerDescription.classList.remove("show");
+    answerDescription.innerHTML = "";
+}
+
+function showNextQuestionBtn() {
+    nextQuestionBtn.classList.add("show");
+}
+
+function hideNextQuestionBtn() {
+    nextQuestionBtn.classList.remove("show");
+}
+
+function showTimeUpText () {
+    timeUpText.classList.add("show");
+}
+
+function hideTimeUpText () {
+    timeUpText.classList.remove("show");
+}
+
+function scoreBoard() {
+    correctAnswers.innerHTML = score;
+}
+
+nextQuestionBtn.addEventListener("click", nextQuestion);
+
+function nextQuestion() {
+    // questionIndex++;
+    generateRandomQuestion();
+    // load();
+    hideNextQuestionBtn();
+    hideAnswerDescription();
+    hideTimeUpText();
+    startTimer();
+}
+
+function quizResult() {
+    document.querySelector(".total-questions").innerHTML = myApp.length;
+    document.querySelector(".total-attempt").innerHTML = attempt;
+    document.querySelector(".total-correct").innerHTML = score;
+    document.querySelector(".total-wrong").innerHTML = attempt-score;
+    const percentage = (score/(myApp.length))*100;
+    document.querySelector(".percentage").innerHTML = Math.floor(percentage) + "%";
+}
+
+function resetQuiz() {
+attempt = 0;
+// questionIndex = 0;
+score = 0;
+number = 0;
+myArray = [];
+}
+
+function quizOver() {
+    nextQuestionBtn.classList.remove("show");
+    seeResultBtn.classList.add("show");
+}
+
+seeResultBtn.addEventListener("click", ()=> {
+    // quizBox.style.display = "none";
+    quizBox.classList.remove("show");
+    seeResultBtn.classList.remove("show");
+    quizOverBox.classList.add("show");
+    quizResult();
+})
+
+startAgainQuizBtn.addEventListener("click", ()=>{
+    quizBox.classList.add("show");
+    quizOverBox.classList.remove("show");
+    resetQuiz();
+    nextQuestion();
+})
+
+goHomeBtn.addEventListener("click", ()=>{
+    quizOverBox.classList.remove("show");
+    quizHomeBox.classList.add("show");
+    resetQuiz();
+})
+
+startQuizBtn.addEventListener("click", ()=>{
+    quizBox.classList.add("show");
+    quizHomeBox.classList.remove("show");
+    startTimer();
+    generateRandomQuestion();
+})
+
+// // window.onload = () => {
+//     // load();
+//     startTimer();
+//     generateRandomQuestion();
+// }
