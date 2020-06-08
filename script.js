9cc6cd84504d6af7ac823abc5a633f2a263803d8
roukell <62136980+roukell@users.noreply.github.com>
2020-06-08 14:05:16 +1000

    updated css

--- script.js
+++ script.js
@@ -22,21 +22,57 @@ let myArray = [];
 let interval;
 
 const myApp = [{
-    question: "Name the smallest type of tree in the world.",
-    options: ["Cork tree", "Bonsai tree", "Hazel", "Black walnut"],
-    answer: 1,
-    description: "Ha",
+    question: "What is the browser's internal representation of your web page?",
+    options: ["Document Object Model", "Domain Object Model", "Document Observation Model", "Domestic Object Model"],
+    answer: 0,
+    description: "Document Object Model (DOM) is the browser's internal representation of your web page.",
 }, {
-    question: "Webpages are written in?",
+    question: "Web pages are written in?",
     options: ["FTP", "HTML", "HTTP", "URL"],
     answer: 1,
-    description: "Ho",
+    description: "Web pages are written in HTML.",
 }, {
-    question: "C is?",
-    options: ["A third generation of high level langauge", "A machine langauge", "An assembly langauge", "All of above"],
+    question: "You can get the value of an element's attributes using ...... method.",
+    options: ["setAttribute method", "getElementById method", "getAttribute method", "All of above"],
+    answer: 2,
+    description: "You can get the value of an element's attributes using getAttribute method",
+}, {
+    question: "What property holds the text content, as well as all nested HTML content, of an element?",
+    options: ["innerContent", "innerHTML", "innerWidth", "innerHeight"],
+    answer: 1,
+    description: "The answer is innerHTML.",
+}, {
+    question: "The document.getElementById method grabs an element from the DOM using its ......?",
+    options: ["Class name", "Attribute", "Id", "Object"],
+    answer: 2,
+    description: "The answer is Id.",
+}, {
+    question: "You can set the value of an element's attributes using ...... method?",
+    options: ["setAttribute method", "getElementById method", "getAttribute method", "All of above"],
     answer: 0,
-    description: "He",
-}]
+    description: "You can set the value of an element's attributes using setAttribute method",
+}, {
+    question: "You can use the window object's ....... property to set an event handler, or callback, function for the load event.",
+    options: ["onload", "load", "addEventListener", "querySelector"],
+    answer: 0,
+    description: "The onload property allows us to set an event handler or callback function for the load event.",
+}, {
+    question: "A ........ is a function expression that has no name.",
+    options: ["anonymous function", "function declaration", "function name", "function reference"],
+    answer: 0,
+    description: "Anonymous function is a function expression that has no name.",
+}, {
+    question: "...... are functions defined inside another function.",
+    options: ["Internal functions", "Multiple functions", "Function home", "Nested functions"],
+    answer: 3,
+    description: "Nested functions are functions defined inside antoehr function.",
+}, {
+    question: "The ...... object is always at the top of the DOM tree.",
+    options: ["head", "body", "HTML", "document"],
+    answer: 3,
+    description: "Document is always at the tope of the DOM tree.",
+}
+]
 
 
 function load() {
