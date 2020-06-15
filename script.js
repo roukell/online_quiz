be335e97d34a7744e2f9c22ca2c70ccbb804e901
roukell <62136980+roukell@users.noreply.github.com>
2020-06-15 23:01:44 +1000

    fixed broken code when localstorage is null

--- script.js
+++ script.js
@@ -29,52 +29,52 @@ const myApp = [{
     answer: 0,
     description: "Document Object Model (DOM) is the browser's internal representation of your web page.",
 }
-, {
-    question: "Web pages are written in?",
-    options: ["FTP", "HTML", "HTTP", "URL"],
-    answer: 1,
-    description: "Web pages are written in HTML.",
-}, {
-    question: "You can get the value of an element's attributes using ...... method.",
-    options: ["setAttribute method", "getElementById method", "getAttribute method", "All of above"],
-    answer: 2,
-    description: "You can get the value of an element's attributes using getAttribute method",
-}, {
-    question: "What property holds the text content, as well as all nested HTML content, of an element?",
-    options: ["innerContent", "innerHTML", "innerWidth", "innerHeight"],
-    answer: 1,
-    description: "The answer is innerHTML.",
-}, {
-    question: "The document.getElementById method grabs an element from the DOM using its ......?",
-    options: ["Class name", "Attribute", "Id", "Object"],
-    answer: 2,
-    description: "The answer is Id.",
-}, {
-    question: "You can set the value of an element's attributes using ...... method?",
-    options: ["setAttribute method", "getElementById method", "getAttribute method", "All of above"],
-    answer: 0,
-    description: "You can set the value of an element's attributes using setAttribute method",
-}, {
-    question: "You can use the window object's ....... property to set an event handler, or callback, function for the load event.",
-    options: ["onload", "load", "addEventListener", "querySelector"],
-    answer: 0,
-    description: "The onload property allows us to set an event handler or callback function for the load event.",
-}, {
-    question: "A ........ is a function expression that has no name.",
-    options: ["anonymous function", "function declaration", "function name", "function reference"],
-    answer: 0,
-    description: "Anonymous function is a function expression that has no name.",
-}, {
-    question: "...... are functions defined inside another function.",
-    options: ["Internal functions", "Multiple functions", "Function home", "Nested functions"],
-    answer: 3,
-    description: "Nested functions are functions defined inside antoehr function.",
-}, {
-    question: "The ...... object is always at the top of the DOM tree.",
-    options: ["head", "body", "HTML", "document"],
-    answer: 3,
-    description: "Document is always at the tope of the DOM tree.",
-}
+// , {
+//     question: "Web pages are written in?",
+//     options: ["FTP", "HTML", "HTTP", "URL"],
+//     answer: 1,
+//     description: "Web pages are written in HTML.",
+// }, {
+//     question: "You can get the value of an element's attributes using ...... method.",
+//     options: ["setAttribute method", "getElementById method", "getAttribute method", "All of above"],
+//     answer: 2,
+//     description: "You can get the value of an element's attributes using getAttribute method",
+// }, {
+//     question: "What property holds the text content, as well as all nested HTML content, of an element?",
+//     options: ["innerContent", "innerHTML", "innerWidth", "innerHeight"],
+//     answer: 1,
+//     description: "The answer is innerHTML.",
+// }, {
+//     question: "The document.getElementById method grabs an element from the DOM using its ......?",
+//     options: ["Class name", "Attribute", "Id", "Object"],
+//     answer: 2,
+//     description: "The answer is Id.",
+// }, {
+//     question: "You can set the value of an element's attributes using ...... method?",
+//     options: ["setAttribute method", "getElementById method", "getAttribute method", "All of above"],
+//     answer: 0,
+//     description: "You can set the value of an element's attributes using setAttribute method",
+// }, {
+//     question: "You can use the window object's ....... property to set an event handler, or callback, function for the load event.",
+//     options: ["onload", "load", "addEventListener", "querySelector"],
+//     answer: 0,
+//     description: "The onload property allows us to set an event handler or callback function for the load event.",
+// }, {
+//     question: "A ........ is a function expression that has no name.",
+//     options: ["anonymous function", "function declaration", "function name", "function reference"],
+//     answer: 0,
+//     description: "Anonymous function is a function expression that has no name.",
+// }, {
+//     question: "...... are functions defined inside another function.",
+//     options: ["Internal functions", "Multiple functions", "Function home", "Nested functions"],
+//     answer: 3,
+//     description: "Nested functions are functions defined inside antoehr function.",
+// }, {
+//     question: "The ...... object is always at the top of the DOM tree.",
+//     options: ["head", "body", "HTML", "document"],
+//     answer: 3,
+//     description: "Document is always at the tope of the DOM tree.",
+// }
 ]
 
 
@@ -281,9 +281,7 @@ let namesAndScoresUser = JSON.parse(localStorage.getItem("namesAndScores"));
 // let userScore = JSON.parse(localStorage.getItem("percentage"));
 
 const record = document.querySelector("#record");
-record.appendChild(createRecord(namesAndScoresUser[0].myName + namesAndScoresUser[0].myScore));
-
-
+record.appendChild(createRecord(namesAndScoresUser));
 
 
 
